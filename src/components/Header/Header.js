6ef1c03a7b8d3a64d4c756f8c2aedd7635961b38
Vasiles Petrides <vasileios.petridis@gmail.com>
2021-01-31 15:06:15 +0200

    built input and positioned

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,16 @@ import styles from './Header.module.css'
 export default function Header() {
   return (
     <div className={styles.container}>
-      <h1>Header</h1>
+      <h1 className={styles.logo}>Logos</h1>
+      <h1 className={styles.title}>love > books</h1>
+      <div className={styles.iconsContainer}>
+        <div className={styles.icon}>
+          <i className="far fa-user"></i>
+        </div>
+        <div className={styles.icon}>
+          <i className="fas fa-shopping-cart"></i>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/SearchBar/SearchItem.js
+++ src/components/SearchBar/SearchItem.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Search.module.css'
 
 export default function SearchItem() {
   return (
-    <div>
-      <input type="text" name="search" id="search" />
+    <div className={styles.searchItem}>
+      <input type="text" name="search" id="search" placeholder="Search for books you'll love..." />
     </div>
   )
 }
